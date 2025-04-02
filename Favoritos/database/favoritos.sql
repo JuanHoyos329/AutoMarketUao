2e473c49dd0fc2b3d4445790fa94e526dae05ebc
juanhoyos1 <juan_andrns.hoyos@uao.edu.co>
2025-04-01 21:28:59 -0500

    Correcion a la base de datos de favoritos

--- Favoritos/database/favoritos.sql
+++ Favoritos/database/favoritos.sql
@@ -1,73 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost:3308
--- Tiempo de generación: 02-04-2025 a las 04:01:54
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `favoritosdb`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `favoritos`
---
-
+CREATE DATABASE favoritos;
+USE favoritos;
 CREATE TABLE `favoritos` (
-  `id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `publicacion_id` int(11) NOT NULL,
-  `fecha_agregado` timestamp NOT NULL DEFAULT current_timestamp()
+    `id` int primary key auto_increment,
+    `userId` int NOT NULL,
+    `publicacion_id` int NOT NULL,
+    `fecha_agregado` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `favoritos`
---
-
-INSERT INTO `favoritos` (`id`, `usuario_id`, `publicacion_id`, `fecha_agregado`) VALUES
-(7, 6, 15, '2025-03-24 17:18:05'),
-(8, 7, 16, '2025-03-24 17:49:22'),
-(9, 8, 17, '2025-03-24 23:27:45'),
-(10, 9, 18, '2025-03-25 22:54:00'),
-(11, 10, 19, '2025-03-25 22:56:09'),
-(12, 11, 20, '2025-03-25 22:56:28'),
-(13, 12, 21, '2025-03-25 22:56:54');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `favoritos`
---
-ALTER TABLE `favoritos`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `favoritos`
---
-ALTER TABLE `favoritos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- User/Databases/config/database.js
+++ User/Databases/config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("usersautomarketuao", "root", "annie", {
+const sequelize = new Sequelize("usersautomarketuao", "root", "root", {
   host: "localhost", // Dirección del servidor de la base de datos
-  port: 3306,
+  //port: 3306,
   dialect: "mysql", // Tipo de base de datos
   dialectOptions: {
     ssl: false, // Equivalente a `useSSL=false` en tu configuración de Java
