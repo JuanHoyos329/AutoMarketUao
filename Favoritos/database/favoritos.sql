62af4c52d2b72e4e21742b42c14a5f8c1527525c
juanhoyos1 <juan_andrns.hoyos@uao.edu.co>
2025-04-01 21:32:49 -0500

    Correcion a la base de datos de favoritos

--- Favoritos/database/favoritos.sql
+++ Favoritos/database/favoritos.sql
@@ -3,6 +3,6 @@ USE favoritos;
 CREATE TABLE `favoritos` (
     `id` int primary key auto_increment,
     `userId` int NOT NULL,
-    `publicacion_id` int NOT NULL,
+    `idPublicacion` int NOT NULL,
     `fecha_agregado` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
